c6afd1073a46bb0050bdcf4e19e4c3a6f3c5bbce
Said <saidziani.pro@gmail.com>
2018-05-10 21:26:03 +0100

    WIP Top 5 articles for you.

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, ImageBackground } from 'react-native';
 import { Toolbar, Button, BottomNavigation, Icon } from 'react-native-material-ui';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import Container from './Container';
@@ -34,214 +34,77 @@ export default class Home extends React.Component{
 
     render(){
         return(
+            <ImageBackground source={require('./bg.jpg')} style={style.backgroundImage} >
             <Container>
                 <ScrollView>
-                    <Grid>
-                        <Row style={style.articleShow}>
-                            <Col size={2} style={style.columnBig}>
-                                <Text style={style.articleTitle}>
-                                    Let's make America Great Again with Donald Trump
-                                </Text>
-                                <Text style={style.articleSummary}>
-                                    Organized for the first time in algeria and trip tu Istanbul...
-                                </Text>
-                                <View>
-                                    <Image style={style.imgS} source={require('../assets/img/articles/1.jpg')} />
-                                    <Text style={style.sourceName}>
-                                        France 24
-                                    </Text>
-                                    <Text style={style.sourceTime}>
-                                        Il y a 5 heures
-                                    </Text>
-                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
-                                    <Icon style={style.interactionIconTwo} name="settings" />
-                                    <Icon style={style.interactionIconThree} name="today" />
+                    <Grid style={{margin:10}}>
+                        <View style={style.welcomePanel}>
+                            <View style={style.hiYou}>
+                                <Text style={style.hiMsg}>Hey Saïd!</Text>
+                                <View style={{marginTop:10,flex: 1,flexDirection: 'row',justifyContent: 'space-between',alignItems: 'center'}}>
+                                    <Text style={{color:'white',fontSize:18}}>Algiers</Text>
+                                    <Icon name={'today'} />
+                                    <Text style={{color:'white',fontSize:18}}>25^C</Text>
                                 </View>
-                            </Col>
-                            <Col size={1} style={style.columnSmall}>
-                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
-                            </Col>
-                        </Row>
-                        <Row style={style.articleShow}>
-                            <Col size={2} style={style.columnBig}>
-                                <Text style={style.articleTitle}>
-                                    Let's make America Great Again with Donald Trump
-                                </Text>
-                                <Text style={style.articleSummary}>
-                                    Organized for the first time in algeria and trip tu Istanbul...
-                                </Text>
-                                <View>
-                                    <Image style={style.imgS} source={require('../assets/img/articles/1.jpg')} />
-                                    <Text style={style.sourceName}>
-                                        France 24
-                                    </Text>
-                                    <Text style={style.sourceTime}>
-                                        Il y a 5 heures
-                                    </Text>
-                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
-                                    <Icon style={style.interactionIconTwo} name="settings" />
-                                    <Icon style={style.interactionIconThree} name="today" />
-                                </View>
-                            </Col>
-                            <Col size={1} style={style.columnSmall}>
-                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
-                            </Col>
-                        </Row>
-                        <Row style={style.articleShow}>
-                            <Col size={2} style={style.columnBig}>
-                                <Text style={style.articleTitle}>
-                                    Let's make America Great Again with Donald Trump
-                                </Text>
-                                <Text style={style.articleSummary}>
-                                    Organized for the first time in algeria and trip tu Istanbul...
-                                </Text>
-                                <View>
-                                    <Image style={style.imgS} source={require('../assets/img/articles/1.jpg')} />
-                                    <Text style={style.sourceName}>
-                                        France 24
-                                    </Text>
-                                    <Text style={style.sourceTime}>
-                                        Il y a 5 heures
-                                    </Text>
-                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
-                                    <Icon style={style.interactionIconTwo} name="settings" />
-                                    <Icon style={style.interactionIconThree} name="today" />
-                                </View>
-                            </Col>
-                            <Col size={1} style={style.columnSmall}>
-                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
-                            </Col>
-                        </Row>
-                        <Row style={style.articleShow}>
-                            <Col size={2} style={style.columnBig}>
-                                <Text style={style.articleTitle}>
-                                    Let's make America Great Again with Donald Trump
-                                </Text>
-                                <Text style={style.articleSummary}>
-                                    Organized for the first time in algeria and trip tu Istanbul...
-                                </Text>
-                                <View>
-                                    <Image style={style.imgS} source={require('../assets/img/articles/1.jpg')} />
-                                    <Text style={style.sourceName}>
-                                        France 24
-                                    </Text>
-                                    <Text style={style.sourceTime}>
-                                        Il y a 5 heures
-                                    </Text>
-                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
-                                    <Icon style={style.interactionIconTwo} name="settings" />
-                                    <Icon style={style.interactionIconThree} name="today" />
-                                </View>
-                            </Col>
-                            <Col size={1} style={style.columnSmall}>
-                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
-                            </Col>
-                        </Row>
-                        <Row style={style.articleShow}>
-                            <Col size={2} style={style.columnBig}>
-                                <Text style={style.articleTitle}>
-                                    Let's make America Great Again with Donald Trump
-                                </Text>
-                                <Text style={style.articleSummary}>
-                                    Organized for the first time in algeria and trip tu Istanbul...
-                                </Text>
-                                <View>
-                                    <Image style={style.imgS} source={require('../assets/img/articles/1.jpg')} />
-                                    <Text style={style.sourceName}>
-                                        France 24
-                                    </Text>
-                                    <Text style={style.sourceTime}>
-                                        Il y a 5 heures
-                                    </Text>
-                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
-                                    <Icon style={style.interactionIconTwo} name="settings" />
-                                    <Icon style={style.interactionIconThree} name="today" />
-                                </View>
-                            </Col>
-                            <Col size={1} style={style.columnSmall}>
-                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
-                            </Col>
-                        </Row>
-                        <Row style={style.articleShow}>
-                            <Col size={2} style={style.columnBig}>
-                                <Text style={style.articleTitle}>
-                                    Let's make America Great Again with Donald Trump
-                                </Text>
-                                <Text style={style.articleSummary}>
-                                    Organized for the first time in algeria and trip tu Istanbul...
-                                </Text>
-                                <View>
-                                    <Image style={style.imgS} source={require('../assets/img/articles/1.jpg')} />
-                                    <Text style={style.sourceName}>
-                                        France 24
-                                    </Text>
-                                    <Text style={style.sourceTime}>
-                                        Il y a 5 heures
-                                    </Text>
-                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
-                                    <Icon style={style.interactionIconTwo} name="settings" />
-                                    <Icon style={style.interactionIconThree} name="today" />
-                                </View>
-                            </Col>
-                            <Col size={1} style={style.columnSmall}>
-                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
-                            </Col>
-                        </Row>
-                        <Row style={style.articleShow}>
-                            <Col size={2} style={style.columnBig}>
-                                <Text style={style.articleTitle}>
-                                    Let's make America Great Again with Donald Trump
-                                </Text>
-                                <Text style={style.articleSummary}>
-                                    Organized for the first time in algeria and trip tu Istanbul...
-                                </Text>
-                                <View>
-                                    <Image style={style.imgS} source={require('../assets/img/articles/1.jpg')} />
-                                    <Text style={style.sourceName}>
-                                        France 24
-                                    </Text>
-                                    <Text style={style.sourceTime}>
-                                        Il y a 5 heures
-                                    </Text>
-                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
-                                    <Icon style={style.interactionIconTwo} name="settings" />
-                                    <Icon style={style.interactionIconThree} name="today" />
-                                </View>
-                            </Col>
-                            <Col size={1} style={style.columnSmall}>
-                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
-                            </Col>
-                        </Row>
-                        <Row style={style.articleShow}>
-                            <Col size={2} style={style.columnBig}>
-                                <Text style={style.articleTitle}>
-                                    Let's make America Great Again with Donald Trump
-                                </Text>
-                                <Text style={style.articleSummary}>
-                                    Organized for the first time in algeria and trip tu Istanbul...
-                                </Text>
-                                <View>
-                                    <Image style={style.imgS} source={require('../assets/img/articles/1.jpg')} />
-                                    <Text style={style.sourceName}>
-                                        France 24
-                                    </Text>
-                                    <Text style={style.sourceTime}>
-                                        Il y a 5 heures
-                                    </Text>
-                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
-                                    <Icon style={style.interactionIconTwo} name="settings" />
-                                    <Icon style={style.interactionIconThree} name="today" />
-                                </View>
-                            </Col>
-                            <Col size={1} style={style.columnSmall}>
-                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
-                            </Col>
-                        </Row>
+                            </View>
+                            <View style={style.topFive}>
+                                <Text style={style.topFiveMsg}>Selection of the day</Text>
+                                <Text style={style.topFiveLittleMsg}>Top 5 articles for you</Text>
+                                <View style={style.topFiveHr}></View>
+                            </View>
 
+                        </View>
 
-
-
-
+                        <Row style={style.articleShow}>
+                            <Col size={2} style={style.columnBig}>
+                                <Text style={style.articleTitle}>
+                                    Let's make America Great Again with Donald Trump
+                                </Text>
+                                <Text style={style.articleSummary}>
+                                    Organized for the first time in algeria and trip tu Istanbul...
+                                </Text>
+                                <View>
+                                    <Image style={style.imgS} source={require('../assets/img/articles/1.jpg')} />
+                                    <Text style={style.sourceName}>
+                                        France 24
+                                    </Text>
+                                    <Text style={style.sourceTime}>
+                                        Il y a 5 heures
+                                    </Text>
+                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
+                                    <Icon style={style.interactionIconTwo} name="settings" />
+                                    <Icon style={style.interactionIconThree} name="today" />
+                                </View>
+                            </Col>
+                            <Col size={1} style={style.columnSmall}>
+                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
+                            </Col>
+                        </Row>
+                        <Row style={style.articleShow}>
+                            <Col size={2} style={style.columnBig}>
+                                <Text style={style.articleTitle}>
+                                    Let's make America Great Again with Donald Trump
+                                </Text>
+                                <Text style={style.articleSummary}>
+                                    Organized for the first time in algeria and trip tu Istanbul...
+                                </Text>
+                                <View>
+                                    <Image style={style.imgS} source={require('../assets/img/articles/1.jpg')} />
+                                    <Text style={style.sourceName}>
+                                        France 24
+                                    </Text>
+                                    <Text style={style.sourceTime}>
+                                        Il y a 5 heures
+                                    </Text>
+                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
+                                    <Icon style={style.interactionIconTwo} name="settings" />
+                                    <Icon style={style.interactionIconThree} name="today" />
+                                </View>
+                            </Col>
+                            <Col size={1} style={style.columnSmall}>
+                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
+                            </Col>
+                        </Row>
 
                     </Grid>
                 </ScrollView>
@@ -272,7 +135,7 @@ export default class Home extends React.Component{
                     />
                 </BottomNavigation>
             </Container>
-
+            </ImageBackground>
 
         )
     }
