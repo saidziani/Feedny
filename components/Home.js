b12836dceebe66a37d2febbb1ba8a4170c2ca31e
Said <saidziani.pro@gmail.com>
2018-05-11 11:50:16 +0100

    Top 5 articles for you.

--- components/Home.js
+++ components/Home.js
@@ -77,6 +77,56 @@ export default class Home extends React.Component{
                                 </View>
                             </Col>
                             <Col size={1} style={style.columnSmall}>
+                                <Image style={style.imgV} source={require('../assets/img/articles/2.jpg')} />
+                            </Col>
+                        </Row>
+                        <Row style={style.articleShow}>
+                            <Col size={2} style={style.columnBig}>
+                                <Text style={style.articleTitle}>
+                                    Let's make America Great Again with Donald Trump
+                                </Text>
+                                <Text style={style.articleSummary}>
+                                    Organized for the first time in algeria and trip tu Istanbul...
+                                </Text>
+                                <View>
+                                    <Image style={style.imgS} source={require('../assets/img/articles/2.jpg')} />
+                                    <Text style={style.sourceName}>
+                                        France 24
+                                    </Text>
+                                    <Text style={style.sourceTime}>
+                                        Il y a 5 heures
+                                    </Text>
+                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
+                                    <Icon style={style.interactionIconTwo} name="settings" />
+                                    <Icon style={style.interactionIconThree} name="today" />
+                                </View>
+                            </Col>
+                            <Col size={1} style={style.columnSmall}>
+                                <Image style={style.imgV} source={require('../assets/img/articles/3.jpg')} />
+                            </Col>
+                        </Row>
+                        <Row style={style.articleShow}>
+                            <Col size={2} style={style.columnBig}>
+                                <Text style={style.articleTitle}>
+                                    Let's make America Great Again with Donald Trump
+                                </Text>
+                                <Text style={style.articleSummary}>
+                                    Organized for the first time in algeria and trip tu Istanbul...
+                                </Text>
+                                <View>
+                                    <Image style={style.imgS} source={require('../assets/img/articles/2.jpg')} />
+                                    <Text style={style.sourceName}>
+                                        France 24
+                                    </Text>
+                                    <Text style={style.sourceTime}>
+                                        Il y a 5 heures
+                                    </Text>
+                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
+                                    <Icon style={style.interactionIconTwo} name="settings" />
+                                    <Icon style={style.interactionIconThree} name="today" />
+                                </View>
+                            </Col>
+                            <Col size={1} style={style.columnSmall}>
                                 <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
                             </Col>
                         </Row>
@@ -102,7 +152,32 @@ export default class Home extends React.Component{
                                 </View>
                             </Col>
                             <Col size={1} style={style.columnSmall}>
-                                <Image style={style.imgV} source={require('../assets/img/articles/1.jpg')} />
+                                <Image style={style.imgV} source={require('../assets/img/articles/2.jpg')} />
+                            </Col>
+                        </Row>
+                        <Row style={style.articleShow}>
+                            <Col size={2} style={style.columnBig}>
+                                <Text style={style.articleTitle}>
+                                    Let's make America Great Again with Donald Trump
+                                </Text>
+                                <Text style={style.articleSummary}>
+                                    Organized for the first time in algeria and trip tu Istanbul...
+                                </Text>
+                                <View>
+                                    <Image style={style.imgS} source={require('../assets/img/articles/2.jpg')} />
+                                    <Text style={style.sourceName}>
+                                        France 24
+                                    </Text>
+                                    <Text style={style.sourceTime}>
+                                        Il y a 5 heures
+                                    </Text>
+                                    <Icon style={style.interactionIconOne} name="bookmark-border" />
+                                    <Icon style={style.interactionIconTwo} name="settings" />
+                                    <Icon style={style.interactionIconThree} name="today" />
+                                </View>
+                            </Col>
+                            <Col size={1} style={style.columnSmall}>
+                                <Image style={style.imgV} source={require('../assets/img/articles/3.jpg')} />
                             </Col>
                         </Row>
 
